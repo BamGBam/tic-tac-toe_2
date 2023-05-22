4932536382dc037b29455b0adc1043198f812756
Bamdad Goshtasbi <bamdadgoshtasbi@gmail.com>
2023-05-21 21:44:45 -0700

    Implemented the squares and notification to show on console

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+<div></div>
+
+
+
+    <div className="App">
+         <h1>Hi Welcome to Tic_Tac_Toe</h1>
+      <header className="App-header">
+        <a
+          className="App-link"
+          href="https://www.exploratorium.edu/explore/puzzles/tictactoe#:~:text=Rules%20for%20Tic%2DTac%2DToe&text=Players%20take%20turns%20putting%20their,game%20ends%20in%20a%20tie."
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Read more about Tic Tac Toe Rules
+        </a>
+      </header>
+    </div>
+
+
+    </div>
+  );
+}
+
+export default App;
